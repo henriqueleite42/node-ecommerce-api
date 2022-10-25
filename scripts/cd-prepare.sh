08c7eb868d94d4c595835954aecf5c399d8b9186
Henrique Leite <henriqueleite616@gmail.com>
2022-10-25 20:36:31 -0300

    Update Script

--- scripts/cd-prepare.sh
+++ scripts/cd-prepare.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+sudo rm -r /var/www/maite-api
+sudo mkdir /var/www/maite-api
+sudo chmod -R 777 /var/www/maite-api
 cd /var/www/maite-api
 sudo yarn install --production=true --frozen-lockfile
--- scripts/cd-start.sh
+++ scripts/cd-start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd /var/www/maite-api
-PM2_HOME=/etc/pm2deamon pm2 delete "MAITE API"
-PM2_HOME=/etc/pm2deamon pm2 start "yarn start" --name "MAITE API"
+
+pm2 delete "MAITE API"
+
+pm2 start "yarn start:api" --name "MAITE API"
