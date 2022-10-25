23f9d8363ed68a932ebd92449573e8d973b54b24
Gabriel Eich <eich.gabriel1@gmail.com>
2022-10-25 19:51:33 -0300

    chore(): add CD configs

--- scripts/cd-prepare.sh
+++ scripts/cd-prepare.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-sudo rm -r /var/www/api
-sudo rm -r /var/www/api-temp
-
-sudo mkdir /var/www/api
-sudo mkdir /var/www/api-temp
+cd /var/www/maite-api
+sudo yarn install --production=true --frozen-lockfile
--- scripts/cd-start.sh
+++ scripts/cd-start.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-
-sudo chmod -R 777 /var/www/api-temp
-sudo chmod -R 777 /var/www/api
-
-cd /var/www/api-temp
-
-yarn
-yarn build:api
-
-sudo mv api/* /var/www/api
-sudo mv package.json /var/www/api
-sudo mv node_modules /var/www/api
-sudo rm -r /var/www/api-temp
-
-cd /var/www/api
-
-pm2 delete api
-pm2 start "yarn start:api" --name "api"
+cd /var/www/maite-api
+PM2_HOME=/etc/pm2deamon pm2 delete "MAITE API"
+PM2_HOME=/etc/pm2deamon pm2 start "yarn start" --name "MAITE API"
